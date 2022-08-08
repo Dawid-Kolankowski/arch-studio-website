ef2ef213a5d56e91f4b2aad293151fdba7bed410
Dawid Kolankowski <nexog1997@gmail.com>
2022-08-08 22:07:48 +0200

    add portfolio and header

---
+++ header.js
@@ -0,0 +1,27 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const body = document.body;
+const y = window.scrollY;
+const overlay = document.querySelector(".overlay");
+
+btnHamburger.addEventListener("click", function () {
+  console.log(overlay);
+  header.classList.toggle("open");
+  body.classList.toggle("no-scroll");
+});
+
+overlay.addEventListener("click", function () {
+  header.classList.toggle("open");
+  body.classList.toggle("no-scroll");
+});
+
+document.addEventListener("scroll", function (e) {
+  if (window.innerWidth < 768) {
+    if (window.scrollY > 120) {
+      header.classList.add("sticky");
+    }
+    if (window.scrollY < 10) {
+      header.classList.remove("sticky");
+    }
+  }
+});
